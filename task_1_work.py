63f7649e2c18774af0892a5f8d9a30e5136485b1
Victoria <45501564+lierariel@users.noreply.github.com>
2020-11-06 10:37:39 +0000

    task 2 part 1 completed
    In the part of task 2, I am asked to create a function that reads text from a file, removes whitespace and returns the text.

---
+++ task_1_work.py
@@ -0,0 +1,26 @@
+# Your code here.
+def get_hits(file_path: str, keyword: str):
+
+    file = open(file_path, "r")
+    text = file.read()
+    file.close()
+
+    hits = text.count(keyword)
+    return hits
+
+def load_rules(path: str): # function that will open file read it and remove white spaces.
+    file = open(path, 'r')
+    text = file.read()
+    spaces_removed = text.strip()
+    split_by_letters = spaces_removed.split('<>')
+    file.close()
+
+    return split_by_letters # return a keyword
+
+
+if __name__ == '__main__':
+    # Your testing here.
+    # Ensure any test code you add is tabbed in as part of this if statement.
+    print(get_hits("C:\\Users\\Victoria_Rouch\\OneDrive - Milton Keynes College O365\\course\\programming\\Task 1\\my_file.txt", "private"))
+    print(load_rules("C:\\Users\\Victoria_Rouch\\OneDrive - Milton Keynes College O365\\course\\programming\\Task 2\\example_1.txt"))
+    
