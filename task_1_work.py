f113b6ae3e803531a70a82f37fa9bd2a54cdb28a
Victoria <45501564+lierariel@users.noreply.github.com>
2020-11-06 10:38:44 +0000

    Update task_1_work.py

--- task_1_work.py
+++ task_1_work.py
@@ -12,7 +12,6 @@ def load_rules(path: str): # function that will open file read it and remove whi
     file = open(path, 'r')
     text = file.read()
     spaces_removed = text.strip()
-    split_by_letters = spaces_removed.split('<>')
     file.close()
 
     return split_by_letters # return a keyword
